8d6994256faa1397444e8b1a9307ff4db302e21e
Justus Dieckmann <justusdieckmann@wwu.de>
2021-12-08 01:58:14 +0100

    MDL-73309 core: fix orphaned calendar subscriptions
    This adds the deletion of calendar subscriptions in the category-,
    course- and group delete methods, and the deletion of calendar events
    in the category delete_move method.
    
    It also retoactively removes all orphaned category events,
    and category-, course- and group calendar subscriptions.

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -4506,5 +4506,30 @@ privatefiles,moodle|/user/files.php';
         upgrade_main_savepoint(true, 2022051000.00);
     }
 
+    if ($oldversion < 2022051900.01) {
+
+        // Delete orphaned category events.
+        $DB->delete_records_subquery('event', 'id', 'id',
+            "SELECT e.id FROM {event} e LEFT JOIN {course_categories} cc ON e.categoryid = cc.id " .
+            "WHERE e.eventtype = 'category' AND cc.id IS NULL");
+
+        // Delete orphaned category event subscriptions.
+        $DB->delete_records_subquery('event_subscriptions', 'id', 'id',
+            "SELECT e.id FROM {event_subscriptions} e LEFT JOIN {course_categories} cc ON e.categoryid = cc.id " .
+            "WHERE e.eventtype = 'category' AND cc.id IS NULL");
+
+        // Delete orphaned course event subscriptions.
+        $DB->delete_records_subquery('event_subscriptions', 'id', 'id',
+            "SELECT e.id FROM {event_subscriptions} e LEFT JOIN {course} c ON e.courseid = c.id " .
+            "WHERE e.eventtype = 'course' AND c.id IS NULL");
+
+        // Delete orphaned group event subscriptions.
+        $DB->delete_records_subquery('event_subscriptions', 'id', 'id',
+            "SELECT e.id FROM {event_subscriptions} e LEFT JOIN {groups} g ON e.groupid = g.id " .
+            "WHERE e.eventtype = 'group' AND g.id IS NULL");
+
+        upgrade_main_savepoint(true, 2022051900.01);
+    }
+
     return true;
 }
