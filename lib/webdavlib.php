c05666c67b83ca8c338ddbb6f0a512a787a51130
Justus Dieckmann <justusdieckmann@wwu.de>
2022-11-24 20:35:28 +0100

    MDL-76472 webdav: Adapt to PHP 8 representing xml parser as class.
    It was previously a resource, which could just be cast to an int,
    but we now use the spl_object_id function get an id for the class.

--- lib/webdavlib.php
+++ lib/webdavlib.php
@@ -640,7 +640,7 @@ class webdav_client {
                     if (strcmp($response['header']['Content-Type'], 'text/xml; charset="utf-8"') == 0) {
                         // ok let's get the content of the xml stuff
                         $this->_parser = xml_parser_create_ns();
-                        $this->_parserid = (int) $this->_parser;
+                        $this->_parserid = $this->get_parser_id($this->_parser);
                         // forget old data...
                         unset($this->_lock[$this->_parserid]);
                         unset($this->_xmltree[$this->_parserid]);
@@ -738,7 +738,7 @@ class webdav_client {
                     if (strcmp($response['header']['Content-Type'], 'text/xml; charset="utf-8"') == 0) {
                         // ok let's get the content of the xml stuff
                         $this->_parser = xml_parser_create_ns();
-                        $this->_parserid = (int) $this->_parser;
+                        $this->_parserid = $this->get_parser_id($this->_parser);
                         // forget old data...
                         unset($this->_delete[$this->_parserid]);
                         unset($this->_xmltree[$this->_parserid]);
@@ -830,7 +830,7 @@ EOD;
                     if (preg_match('#(application|text)/xml;\s?charset=[\'\"]?utf-8[\'\"]?#i', $response['header']['Content-Type'])) {
                         // ok let's get the content of the xml stuff
                         $this->_parser = xml_parser_create_ns('UTF-8');
-                        $this->_parserid = (int) $this->_parser;
+                        $this->_parserid = $this->get_parser_id($this->_parser);
                         // forget old data...
                         unset($this->_ls[$this->_parserid]);
                         unset($this->_xmltree[$this->_parserid]);
@@ -1069,7 +1069,7 @@ EOD;
 
     private function _endElement($parser, $name) {
         // end tag was found...
-        $parserid = (int) $parser;
+        $parserid = $this->get_parser_id($parser);
         $this->_xmltree[$parserid] = substr($this->_xmltree[$parserid],0, strlen($this->_xmltree[$parserid]) - (strlen($name) + 1));
     }
 
@@ -1085,7 +1085,7 @@ EOD;
      */
     private function _propfind_startElement($parser, $name, $attrs) {
         // lower XML Names... maybe break a RFC, don't know ...
-        $parserid = (int) $parser;
+        $parserid = $this->get_parser_id($parser);
 
         $propname = strtolower($name);
         if (!empty($this->_xmltree[$parserid])) {
@@ -1181,7 +1181,7 @@ EOD;
      */
     private function _delete_startElement($parser, $name, $attrs) {
         // lower XML Names... maybe break a RFC, don't know ...
-        $parserid = (int) $parser;
+        $parserid = $this->get_parser_id($parser);
         $propname = strtolower($name);
         $this->_xmltree[$parserid] .= $propname . '_';
 
@@ -1233,7 +1233,7 @@ EOD;
      */
     private function _lock_startElement($parser, $name, $attrs) {
         // lower XML Names... maybe break a RFC, don't know ...
-        $parserid = (int) $parser;
+        $parserid = $this->get_parser_id($parser);
         $propname = strtolower($name);
         $this->_xmltree[$parserid] .= $propname . '_';
 
@@ -1289,7 +1289,7 @@ EOD;
      * @access private
      */
     private function _lock_cData($parser, $cdata) {
-        $parserid = (int) $parser;
+        $parserid = $this->get_parser_id($parser);
         if (trim($cdata) <> '') {
             // $this->_error_log(($this->_xmltree[$parserid]) . '='. htmlentities($cdata));
             $this->_lock_ref_cdata .= $cdata;
@@ -1745,6 +1745,21 @@ EOD;
     private function _error_log($err_string) {
         if ($this->_debug) {
             error_log($err_string);
+        }
+    }
+
+    /**
+     * Returns a valid parser id for both PHP 7 and PHP 8+.
+     * In PHP 7, a XML parser handle is a resource, in PHP 8+ it is an object.
+     *
+     * @param resource|XMLParser $parser
+     * @return int parser id.
+     */
+    private function get_parser_id($parser): int {
+        if (is_object($parser)) {
+            return spl_object_id($parser);
+        } else {
+            return (int) $parser;
         }
     }
 }
